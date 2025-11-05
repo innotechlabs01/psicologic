a9ce2db2b57d9075ac635ea8c2680ee9c82a582b
innotechlabs01 <innotechlabssas@gmail.com>
2025-11-04 20:26:02 -0500

    Development (#26)
    * update deploy (#3)
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#5)
    
    * update deploy
    
    * configure deploy vercel
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#7)
    
    * update deploy
    
    * configure deploy vercel
    
    * change file
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#9)
    
    * update deploy
    
    * configure deploy vercel
    
    * change file
    
    * update date
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#10)
    
    * update deploy
    
    * configure deploy vercel
    
    * change file
    
    * update date
    
    * update validate to user clrek configurate
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#12)
    
    * update deploy
    
    * configure deploy vercel
    
    * change file
    
    * update date
    
    * update validate to user clrek configurate
    
    * Fixed middleware
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl deploy (#14)
    
    * update deploy
    
    * configure deploy vercel
    
    * change file
    
    * update date
    
    * update validate to user clrek configurate
    
    * Fixed middleware
    
    * Ajustes en todo el codigo, especial el menu
    
    * deploy vercel
    
    * delete vercel.json
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * convert string to json (#16)
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Change game to cart and fix show repeat information (#18)
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl review game (#20)
    
    * Change game to cart and fix show repeat information
    
    * update css
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl active payment user (#22)
    
    * Create commit payment
    
    * create payment, feedback, chat message client
    
    * update date changelog
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * Itl active payment user (#24)
    
    * Create commit payment
    
    * create payment, feedback, chat message client
    
    * update date changelog
    
    * fixed paymentStatus
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>
    
    * deploy development
    
    * Fix Vercel deployment configuration
    
    * fixed enviorement
    
    ---------
    
    Co-authored-by: anthony rivera <anthonyrivera51@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import clerk from '@clerk/astro';
 import react from '@astrojs/react';
-import node from '@astrojs/node'; // Para Node standalone
+import vercel from '@astrojs/vercel/serverless'; // Para Vercel
 import tailwindcss from '@tailwindcss/vite';
 import { esES } from '@clerk/localizations';
 import dotenv from 'dotenv';
@@ -12,20 +12,14 @@ export default defineConfig({
   integrations: [
     clerk({
       localization: esES,
-      publishableKey: process.env.CLERK_PUBLISHABLE_KEY,
+      publishableKey: process.env.PUBLIC_CLERK_PUBLISHABLE_KEY,
       secretKey: process.env.CLERK_SECRET_KEY
     }),
     react(),
   ],
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   output: 'server',
   vite: {
     plugins: [tailwindcss()],
-  },
-  server: {
-    allowedHosts: ['74c839f3adfd.ngrok-free.app'], // ✅ permite el host de ngrok
-  },
-  hmr: {
-    clientPort: 443,
   },
 });
